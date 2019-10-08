36b5a00be4fdf61b1b3d307fe23ad50d48eec879
Jsparks1989 <jsparks19891@gmail.com>
2019-10-08 19:23:38 -0400

    More changes to make sure there are no more issues.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-import 'react-dom'
+import 'react-dom';
 
 console.log('Loaded react-dom');
 
 console.log('Webpack works!!');
 
 console.log('Webpack still works!!');
+
+console.log('Webpack is watching what I do...');
