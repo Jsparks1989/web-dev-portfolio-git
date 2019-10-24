21fa33c3ecd5a76b5edb3462c83cd297ac828dcb
Jsparks1989 <jsparks19891@gmail.com>
2019-10-24 16:40:45 -0400

    Exported ExampleWorkBubble component; Checked to make sure that ExampleWorkBubble component had only 1 img element.

--- __tests__/test-example-work.js
+++ __tests__/test-example-work.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow }from 'enzyme';
-import ExampleWork from '../js/example-work';
+import ExampleWork, { ExampleWorkBubble } from '../js/example-work';
 
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -25,18 +25,40 @@ const myWork = [
     }
 ];
 
+// //-- ExampleWork Component
+// describe('ExampleWork Component', () => {
+//     //-- shallow imported from enzyme. Taking JSX tag as parameter.
+//     let component = shallow(<ExampleWork work={myWork}/>);
 
-describe('ExampleWork Component', () => {
-    //-- shallow imported from enzyme. Taking JSX tag as parameter.
-    let component = shallow(<ExampleWork work={myWork}/>);
+//     it("Should be a 'section' element", () => {
+//         expect(component.type()).toEqual('section');
+//     });
 
-    it("Should be a 'section' element", () => {
-        expect(component.type()).toEqual('section');
+//     it("Should contain as many children as there are work examples", () => {
+//         expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length)
+//     });
+// });
+
+
+
+
+
+
+//-- ExampleWorkBubble Component
+describe('ExampleWorkBubble Component', () => {
+    let component = shallow(<ExampleWorkBubble example={myWork[1]} />);
+
+    it("Should be a 'div' element", () => {
+        expect(component.type()).toEqual('div');
     });
 
-    //-- find() will find children(ExampleWorkBubble) of a component(ExampleWork); returns an array.
-    //-- expecting the length of the component.find() array to be equal to the myWork array.
-    it("Should contain as many children as there are work examples", () => {
-        expect(component.find("ExampleWorkBubble").length).toEqual(myWork.length)
+    //-- finding all the img elements inside the ExampleWorkBubble component. 
+    //-- 'images' will be an array of all the img elements found in the ExampleWorkBubble component.
+    let images = component.find("img");
+
+    //-- find the image inside of the bubble.
+    it('Should contain a single image element', () => {
+        //-- Expecting the 'images' array to only have one object
+        expect(images.length).toEqual(1);
     });
 });
