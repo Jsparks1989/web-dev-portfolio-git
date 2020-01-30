9412ef7163710c048af5e91ee63ec5e3c161f8b4
Jsparks1989 <jsparks19891@gmail.com>
2020-01-30 02:09:27 -0500

    It might be the browser cache... I hope so.

--- js/main.js
+++ js/main.js
@@ -6,61 +6,61 @@ import ExampleWork from './example-work';
 //-- This is the data that changes for each work example.
 //-- Will be a list of object literals
 const myWork = [
+    {
+        'title': "Wedding",
+        'href': "http://52.203.64.139",
+        'desc': "Wedding website created in Wordpress. Site is hosted in AWS.",
+        'image': {
+            'desc': "Wedding website I created with Wordpress",
+            'src': "images/kimmie-milo-wedding-portfolio.png"
+        }
+    },
+    {
+        'title': "Buy AC",
+        'href': "http://www.buyac.com/",
+        'desc': "This is filler text for Buy AC. I will change it later.",
+        'image': {
+            'desc': "Ecommerce site I created for an HVAC company",
+            'src': "images/buy_ac_thumb_portfolio.png"
+        }
+    }
     // {
-    //     'title': "Wedding",
-    //     'href': "http://52.203.64.139",
-    //     'desc': "Wedding website created in Wordpress. Site is hosted in AWS.",
+    //     'title': "Ecommerce",
+    //     'href': "52.203.64.139",
+    //     'desc': "This is filler text for Ecommerce. I will change it later.",
     //     'image': {
-    //         'desc': "Wedding website I created with Wordpress",
-    //         'src': "images/kimmie-milo-wedding-portfolio.png"
-    //     }
-    // },
-    // {
-    //     'title': "Buy AC",
-    //     'href': "http://www.buyac.com/",
-    //     'desc': "This is filler text for Buy AC. I will change it later.",
-    //     'image': {
-    //         'desc': "Ecommerce site I created for an HVAC company",
-    //         'src': "images/buy_ac_thumb_portfolio.png"
+    //         'desc': "Mock ecommerce site I created",
+    //         'src': "images/needs-n-likes-portfolio.png"
     //     }
     // }
-    // // {
-    // //     'title': "Ecommerce",
-    // //     'href': "52.203.64.139",
-    // //     'desc': "This is filler text for Ecommerce. I will change it later.",
-    // //     'image': {
-    // //         'desc': "Mock ecommerce site I created",
-    // //         'src': "images/needs-n-likes-portfolio.png"
-    // //     }
-    // // }
-    // ,{
-    //     'title': "NHCEF",
-    //     'href': "http://nhcef.org/",
-    //     'desc': "North Hall Community Education Foundation description",
-    //     'image': {
-    //         'desc': "North Hall Community Education Foundation image description",
-    //         'src': "images/nhcef_logo.png"
-    //     }
-    // },{
-    //     'title': "PHP Blog",
-    //     'href': "http://justin-cms.com",
-    //     'desc': "Blogging website done in php",
-    //     'image': {
-    //         'desc': "Image of Justin PHP blog site",
-    //         'src': "images/php_cms_image.png"
-    //     }
+    ,{
+        'title': "NHCEF",
+        'href': "http://nhcef.org/",
+        'desc': "North Hall Community Education Foundation description",
+        'image': {
+            'desc': "North Hall Community Education Foundation image description",
+            'src': "images/nhcef_logo.png"
+        }
+    },{
+        'title': "PHP Blog",
+        'href': "http://justin-cms.com",
+        'desc': "Blogging website done in php",
+        'image': {
+            'desc': "Image of Justin PHP blog site",
+            'src': "images/php_cms_image.png"
+        }
         
-    // },{
+    },{
         
-    //     'title': "Consultants-Lab",
-    //     'href': "https://consultants-lab.com/",
-    //     'desc': "Description of Consultants-Lab website",
-    //     'image': {
-    //         'desc': "Image of Consultants-Lab image",
-    //         'src': "images/medical_source_thumb.png"
-    //     }
+        'title': "Consultants-Lab",
+        'href': "https://consultants-lab.com/",
+        'desc': "Description of Consultants-Lab website",
+        'image': {
+            'desc': "Image of Consultants-Lab image",
+            'src': "images/medical_source_thumb.png"
+        }
 
-    // }
+    }
 ]
 
 //-- Goes in index.html div#id example-work; work is a props
